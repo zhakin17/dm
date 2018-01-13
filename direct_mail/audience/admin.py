2e4c71e2a344db7923e763ac08e1a7a79729a204
zhakin17 <zhakin17@gmail.com>
2018-01-13 09:58:24 +0600

    second commit

---
+++ direct_mail/audience/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Audience
+
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
+
+
+
+@admin.register(Audience)
+class AudienceAdmin(TreeAdmin):
+    form = movenodeform_factory(Audience)
+
+#admin.site.register(MyNode, MyAdmin) eto ravnozna4no @admin.register(Audience)
---
+++ direct_mail/delivery_letter/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DeliveryLetterConfig(AppConfig):
+    name = 'direct_mail.delivery_letter'
